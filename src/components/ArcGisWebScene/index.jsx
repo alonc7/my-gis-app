02e268df1b05e89d76ae41458830d48e406c33af
alonc7 <kochmanstudent@gmail.com>
2024-08-21 23:04:35 +0300

    Updated ArcGISWebScene.index
    Replaced logic of static utils.cities with dynamic fetch from ArcGIS services.

--- src/components/ArcGisWebScene/index.jsx
+++ src/components/ArcGisWebScene/index.jsx
@@ -1,21 +1,15 @@
 import MapView from "@arcgis/core/views/MapView";
 import WebMap from "@arcgis/core/WebMap.js";
-import Graphic from "@arcgis/core/Graphic";
-import PopupTemplate from "@arcgis/core/PopupTemplate";
-import Point from "@arcgis/core/geometry/Point";
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 import { useEffect, useRef } from "react";
 import "./styles.css";
-import { cities } from "../../utils/cities";
 
 const ArcGisWebMap = () => {
     const mapRef = useRef(null);
 
     useEffect(() => {
         const webMap = new WebMap({
-            // portalItem: {
-            //     id: "e691172598f04ea8881cd2a4adaa45ba"
-            // }
-            basemap:'topo'
+            basemap: 'topo'
         });
 
         const mapView = new MapView({
@@ -25,36 +19,16 @@ const ArcGisWebMap = () => {
             zoom: 10
         });
 
-        // Array of capital cities with their coordinates and population
-
-        // Loop through the cities and add them as graphics to the map
-        cities.forEach((city) => {
-            const point = new Point({
-                longitude: city.coordinates[0],
-                latitude: city.coordinates[1]
-            });
-
-            const popupTemplate = new PopupTemplate({
-                title: city.name,
-                content: `Population: ${city.population}`
-            });
-
-            const cityGraphic = new Graphic({
-                geometry: point,
-                symbol: {
-                    type: "simple-marker",
-                    color: "red",
-                    size: "8px",
-                    outline: {
-                        color: [255, 255, 255],
-                        width: 1
-                    }
-                },
-                popupTemplate: popupTemplate
-            });
-
-            mapView.graphics.add(cityGraphic);
+        const citiesLayer = new FeatureLayer({
+            url: "https://services.arcgis.com/P3ePLMYs2RVChkJx/ArcGIS/rest/services/World_Cities/FeatureServer/0",
+            outFields: ["*"],
+            popupTemplate: {
+                title: "{CITY_NAME}",
+                content: "Population: {POP}"
+            }
         });
+
+        webMap.add(citiesLayer);
 
         return () => {
             if (mapView) {
